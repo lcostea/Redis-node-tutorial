7ae97bbf03cdb03284ce10c2f6a452bc7957e91f
Liviu Costea <email.lcostea@gmail.com>
2016-01-17 12:17:03 +0200

    adding fakeredis for my tests - works ok, kinda slow one

--- test/userTest.js
+++ test/userTest.js
@@ -3,7 +3,7 @@
 var chai = require('chai'),
 	sinon = require('sinon'),
 	expect = chai.expect,
-    redis = require('redis'),
+    fakeRedis = require('fakeredis'),
     User = require('../User'),
     UserRepository = require('../userRepository');
     
@@ -12,80 +12,35 @@ chai.should();
 
 
 describe('Test the creation of a user', function() {
-//     it('In case everything goes ok hmset from redis should be called', function(done) {
-//         var redisClient = redis.createClient();
-//         
-//         var infoStub = sinon.stub(redisClient, "info");
-//         infoStub.callsArg(0);
-//         
-//         var hmsetStub = sinon.stub(redisClient, "hmset");
-//         hmsetStub.callsArg(2);
-//         
-//         var lpushStub = sinon.stub(redisClient, "lpush");
-//         
-//         var userRepository = UserRepository.create(redisClient);
-//         
-//         var user = User.create("John", "Smith", "johnny@smith.com", "Bucharest", "Romania", "Software Tester", "Best Company");     
-// 
-//         userRepository.save(user, function() {
-//             expect(infoStub.called).to.be.true;
-//             done();
-//         });
-//         
-//         
-//    });  
-   it('Return a valid user when everything goes ok', function(done) {
-        var redisClient = redis.createClient();
+   it('Save a user and load it back', function(done) {
+        var fakeRedisClient = fakeRedis.createClient("Fake Redis");
         
-        
-        var userRepository = UserRepository.create(redisClient);
+        var userRepository = UserRepository.create(fakeRedisClient);
         
         var expectedUser = User.create("Dev", "Experience", "devexperiencero@gmail.com", "Iasi", "Romania", "Software Developer", "DevExperience");
         
-        var hgetallResponse = {
-            FirstName: 'Dev',
-            LastName: 'Experience',
-            City: 'Iasi',
-            Country: 'Romania',
-            JobTitle: 'Software Developer',
-            JobCompany: 'DevExperience'
-        };
-        
-        
-        var infoStub = sinon.stub(redisClient, "info");
-        infoStub.callsArg(0);
-
-        
-        var hgetallUserStub = sinon.stub(redisClient, "hgetall");
-        hgetallUserStub.yields(null, hgetallResponse);
-        
-
-        userRepository.getUser("devexperiencero@gmail.com", function(actualUser) {
-            hgetallUserStub.restore();
-            expect(expectedUser.emailAddress).to.equal(actualUser.emailAddress);
-            expect(expectedUser.firstName).to.equal(actualUser.firstName);
-            expect(expectedUser.lastName).to.equal(actualUser.lastName);
-            expect(expectedUser.city).to.equal(actualUser.city);
-            expect(expectedUser.country).to.equal(actualUser.country);
-            expect(expectedUser.jobTitle).to.equal(actualUser.jobTitle);
-            expect(expectedUser.jobCompany).to.equal(actualUser.jobCompany);
+        userRepository.save(expectedUser, function(err, reply) {
             
-            done();
+            userRepository.getUser("devexperiencero@gmail.com", function(actualUser) {
+                expect(expectedUser.emailAddress).to.equal(actualUser.emailAddress);
+                expect(expectedUser.firstName).to.equal(actualUser.firstName);
+                expect(expectedUser.lastName).to.equal(actualUser.lastName);
+                expect(expectedUser.city).to.equal(actualUser.city);
+                expect(expectedUser.country).to.equal(actualUser.country);
+                expect(expectedUser.jobTitle).to.equal(actualUser.jobTitle);
+                expect(expectedUser.jobCompany).to.equal(actualUser.jobCompany);
+                
+                done();
+            });
         });
+        
+        
    });
    
    it('Dont return anything when the user doesnt exists', function(done) {
-       var redisClient = redis.createClient();
+       var fakeRedisClient = fakeRedis.createClient("Fake Redis");
         
-        var userRepository = UserRepository.create(redisClient);        
-        
-        var infoStub = sinon.stub(redisClient, "info");
-        infoStub.callsArg(0);
-
-        
-        var hgetallUserStub = sinon.stub(redisClient, "hgetall");
-        hgetallUserStub.yields(null, null);
-        
+        var userRepository = UserRepository.create(fakeRedisClient);
 
         userRepository.getUser("test@test.com", function(actualUser) {
             expect(Object.keys(actualUser).length).to.equal(0);
