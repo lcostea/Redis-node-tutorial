6d3813e4e31738f405b3d49476d9123ebdc096c0
Liviu Costea <Liviu Costea>
2016-09-11 12:54:26 +0300

    general name

--- test/postTest.js
+++ test/postTest.js
@@ -20,7 +20,7 @@ describe('Test the creation of a user post', function() {
      var postRepository = PostRepository.create(fakeRedisClient);
      var creationDate = Date.now();
 
-     var firstPost = Post.create("first@gmail.com", "Voxxed Days Bucharest 2016", "Voxxed Days Bucharest 2016 - seems really interesting", creationDate, 0);
+     var firstPost = Post.create("first@gmail.com", "Voxxed Days Belgrade 2016", "Voxxed Days Belgrade 2016 - seems really interesting", creationDate, 0);
 
      postRepository.save(firstPost, function(err, reply) {
 			 	var allOperationsToDo = 4;
@@ -37,30 +37,30 @@ describe('Test the creation of a user post', function() {
         });
 
 				postRepository.getLastCreatedPosts(1, function(lastCreatedPostsList) {
-					expect(lastCreatedPostsList[0].title).to.equal('Voxxed Days Bucharest 2016');
+					expect(lastCreatedPostsList[0].title).to.equal('Voxxed Days Belgrade 2016');
 					expect(lastCreatedPostsList[0].key).to.equal(postHashKey);
 					allOperationsToDo--;
 					areOperationsDone(allOperationsToDo, done);
 				});
 
 				postRepository.getLastPostsPerUser(1, firstPost.emailAddress, function(postPerUserList) {
-					expect(postPerUserList[0].title).to.equal('Voxxed Days Bucharest 2016');
-					expect(postPerUserList[0].key).to.equal(postRepository.getPostsHashKey(firstPost.emailAddress, 'Voxxed Days Bucharest 2016'));
+					expect(postPerUserList[0].title).to.equal('Voxxed Days Belgrade 2016');
+					expect(postPerUserList[0].key).to.equal(postRepository.getPostsHashKey(firstPost.emailAddress, 'Voxxed Days Belgrade 2016'));
 					allOperationsToDo--;
 					areOperationsDone(allOperationsToDo, done);
 				});
 
 				postRepository.getTopVotedPosts(1, function(topVotedPostsList) {
-					expect(topVotedPostsList[0].title).to.equal('Voxxed Days Bucharest 2016');
+					expect(topVotedPostsList[0].title).to.equal('Voxxed Days Belgrade 2016');
 					expect(topVotedPostsList[0].votes).to.equal(0);
-					expect(topVotedPostsList[0].key).to.equal(postRepository.getPostsHashKey(firstPost.emailAddress, 'Voxxed Days Bucharest 2016'));
+					expect(topVotedPostsList[0].key).to.equal(postRepository.getPostsHashKey(firstPost.emailAddress, 'Voxxed Days Belgrade 2016'));
 					allOperationsToDo--;
 					areOperationsDone(allOperationsToDo, done);
 				});
 
 				postRepository.getPosts(1, function(postsList) {
-					expect(postsList[0].title).to.equal('Voxxed Days Bucharest 2016');
-					expect(postsList[0].key).to.equal(postRepository.getPostsHashKey(firstPost.emailAddress, 'Voxxed Days Bucharest 2016'));
+					expect(postsList[0].title).to.equal('Voxxed Days Belgrade 2016');
+					expect(postsList[0].key).to.equal(postRepository.getPostsHashKey(firstPost.emailAddress, 'Voxxed Days Belgrade 2016'));
 					allOperationsToDo--;
 					areOperationsDone(allOperationsToDo, done);
 				});
